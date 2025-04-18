0eb58f4092e6a1b557c3e59d98b50d5e53184d53
Kunal <virajprajapat854@gmail.com>
2025-04-19 00:04:07 +0530

    add code

--- Arrays/src/in/kunal/Second_Largest_Element.java
+++ Arrays/src/in/kunal/Second_Largest_Element.java
@@ -1,11 +1,30 @@
 package in.kunal;
 
 public class Second_Largest_Element {
-	
-	public static void main(String[] args) {
-		
+
+	static int secondlargest(int arr[]) {
+
+		int first = Integer.MIN_VALUE;
+
+		int second = Integer.MIN_VALUE;
+
+		int n = arr.length;
+
+		for (int num : arr)
+			if (num > first) {
+				second = first;
+				first = num;
+			} else if (num > second && num < first) {
+				second = num;
+
+			}
+		return second == Integer.MIN_VALUE ? -1 : second;
+
 	}
 
+	public static void main(String[] args) {
+		int arr[] = { 10, 12, 15 };
+		int secondlargest = secondlargest(arr);
+		System.out.println(secondlargest);
+	}
 }
-
-
